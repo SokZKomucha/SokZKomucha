843055fa848142e8390cd2736ddb0b62805bd2ef
CCl₂F₂ <74624947+SokZKomucha@users.noreply.github.com>
2022-12-03 00:05:41 +0100

    Add files via upload

---
+++ C++/CePlusPlus-22.cpp
@@ -0,0 +1,48 @@
+#include <iostream>
+#include <stdlib.h>
+#include <time.h>
+
+#define niePodpisany unsigned
+#define zmiennaCalkowita int
+#define zmiennaCalkowitaDluga long
+#define zmiennaCalkowitaKrotka short
+#define zmiennaZmiennoprzecinkowa float
+#define zmiennaZmiennoprzecinkowaPodwojna double
+#define znak char
+#define sznurek string
+#define binarny bool
+#define proznia void
+#define glowny main
+
+#define staly const
+#define prawda true
+#define falsz false
+
+#define wypisz cout 
+#define wczytaj cin 
+#define nowaLinia endl 
+
+#define jezeli if 
+#define wPrzeciwnymWypadku else
+#define przelacz switch
+#define wypadek case
+
+#define podczas while
+#define wykonaj do
+#define podczasGdy for
+
+#define zwrot return
+#define idzDo goto
+#define zniszcz break
+
+
+
+using namespace std;
+
+zmiennaCalkowita glowny()
+{
+
+    // Kod tutaj
+    
+    zwrot 0;
+}
